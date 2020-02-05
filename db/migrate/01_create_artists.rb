fce6e3e8090b1b5fa044a716d2706643eb4f0214
Daniel Reina <dannyflatiron@gmail.com>
2020-02-05 01:06:01 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -8,4 +8,14 @@ class CreateArtists < ActiveRecord::Migration[5.2]
     # code to execute when the migration is rolled back
   end
 
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+
+
 end
